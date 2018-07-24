d4e712c5f8d0e8f39d3634cb40f3e4805502e273
Max Huang <sakana@cycu.org.tw>
2018-07-24 21:12:41 +0800

    update reading 20180724

---
+++ mindmap/kubernetes20180724.mm
@@ -0,0 +1,140 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1532174397964" ID="ID_1401321385" MODIFIED="1532434847234" STYLE="bubble" TEXT="kubernetes">
+<font NAME="Serif" SIZE="12"/>
+<node CREATED="1532434809079" ID="ID_297862997" MODIFIED="1532434953408" POSITION="right" STYLE="bubble" TEXT="kubectl">
+<font NAME="Serif" SIZE="12"/>
+<node CREATED="1532434913439" ID="ID_418994478" MODIFIED="1532434953408" TEXT="version">
+<font NAME="Serif" SIZE="12"/>
+<node CREATED="1532434922070" ID="ID_519104100" MODIFIED="1532434953400">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#27298;&#26597;&#24037;&#20855;&#36996;&#26377;&#20282;&#26381;&#22120;&#30340;&#29256;&#26412;
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1532435574564" ID="ID_710648591" MODIFIED="1532435625658" TEXT="get">
+<font NAME="Serif" SIZE="12"/>
+<node CREATED="1532435679279" ID="ID_1500676531" MODIFIED="1532435702615">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      componentstatuses
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1532435706806" ID="ID_1964581453" MODIFIED="1532435729169">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#27298;&#26597;&#21474;&#38598;&#20581;&#24247;&#29376;&#24907;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1532435926549" ID="ID_1466680385" MODIFIED="1532435928055" TEXT="nodes">
+<node CREATED="1532435929836" ID="ID_269098384" MODIFIED="1532435945367" TEXT="&#x5217;&#x51fa;kubernetes&#x7684;&#x5de5;&#x4f5c;&#x7bc0;&#x9ede;"/>
+</node>
+<node CREATED="1532436826829" ID="ID_1764440145" MODIFIED="1532436831568" TEXT="daemonSets">
+<node CREATED="1532436844972" ID="ID_113592718" MODIFIED="1532436927006">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21015;&#20986;&#27491;&#22312;&#36939;&#20316;&#30340; kube-proxy
+    </p>
+    <p>
+      # kubectl&#160;&#160;get&#160;&#160;daemonSets&#160;&#160;--namespace=kube-system kube-proxy
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1532437083162" ID="ID_1929528753" MODIFIED="1532437088870" TEXT="deployments">
+<node CREATED="1532437101558" ID="ID_12034208" MODIFIED="1532437164231">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21015;&#20986; deployments &#21487;&#20197;&#25351;&#23450; namespace
+    </p>
+    <p>
+      &#20363;&#22914;
+    </p>
+    <p>
+      # kubectl&#160;&#160;get&#160;&#160;deployments --namespace=kube-system
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1532437340417" ID="ID_1074130957" MODIFIED="1532437344511" TEXT="services">
+<node CREATED="1532437353047" ID="ID_1485024508" MODIFIED="1532437387028">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21015;&#20986; services
+    </p>
+    <p>
+      # kubectl get services --namespace=kube-system
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1532436143158" ID="ID_1693829664" MODIFIED="1532436146519" TEXT="describe">
+<node CREATED="1532436148893" ID="ID_1879036778" MODIFIED="1532436150219" TEXT="nodes">
+<node CREATED="1532436154293" ID="ID_1477033167" MODIFIED="1532436233621">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21015;&#20986;&#31680;&#40670;&#36039;&#35338;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      &#38928;&#35373;&#21015;&#20986;&#20840;&#37096;,&#20063;&#21487;&#20197;&#37341;&#23565;&#29305;&#23450;&#31680;&#40670;&#21015;&#20986;
+    </p>
+    <p>
+      # kubectl describe&#160;&#160;node&#160;&#160;node-1
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
--- other_materials/Max_Huang/Kubernetes_up_and_running/README.md
+++ other_materials/Max_Huang/Kubernetes_up_and_running/README.md
@@ -2,6 +2,19 @@ This is notes for Max reading
 
 -------------------------------------
 
+2018/7/24
+
+進度: Chapter 2 ~ Chapter 3 結束
+
+* https://github.com/kubernetes-up-and-running/kuard
+
+* 使用 az aks 建立 kubernetes 叢集, 如果沒有指定 --node-count x , 預設會啟動 3 台機器
+
+* Standard_F1s 對於實驗來說應該是比較划算的 NT$ 1408.7 / 月
+
+
+-------------------------------------
+
 2018/7/21
 
 進度:  Chapter 1 ~ Chapter 1 結束
