1d43238983acf984c77df78161c41668cf0a5f77
Max Huang <sakana@cycu.org.tw>
2022-02-13 14:55:34 +0800

    更新 刪除 aks shell script

--- other_materials/Max_Huang/Kubernetes_up_and_running_2nd_edition/task-shell-script/azure_delete_AKS.sh
+++ other_materials/Max_Huang/Kubernetes_up_and_running_2nd_edition/task-shell-script/azure_delete_AKS.sh
@@ -7,14 +7,13 @@ echo ""
 echo "刪除 Azure AKS"
 echo ""
 
-
-# 列出Resource Group
+# 列出 AKS Cluster
 echo ""
-echo "列出可用的 Resource Group"
-echo "如果已經知道要設定的 Resource Group 名稱, 可以不列出 Resource Group list"
-read -e -p "是否要列出所有 Resource Group? (要列出所有 Resource Group 請輸入小寫 yes) : " -i "no" list_resource_groups
+echo "列出 AKS Cluster"
+echo "如果已經知道要刪除的 AKS Cluster 名稱, Resource Group 名稱, 可以不列出 AKS Cluster list"
+read -e -p "是否要列出所有 AKS Cluster? (要列出所有 AKS Cluster 請輸入小寫 yes) : " -i "no" list_aks_clusters
 echo ""
-[ $list_resource_groups == yes ] && az group list --output table
+[ $list_aks_clusters == yes ] && az aks list --output table
 echo ""
 
 # 設定 Resource Group 
@@ -22,16 +21,6 @@ echo ""
 echo "設定要刪除 AKS 的 Resource Group"
 read -e -p "Please enter AKS Resource Group name: " -i "test-sakana" RG_name
 echo ""
-
-# 列出 AKS Cluster
-echo ""
-echo "列出 AKS Cluster"
-echo "如果已經知道要刪除的 AKS Cluster 名稱, 可以不列出 AKS Cluster list"
-read -e -p "是否要列出所有 AKS Cluster? (要列出所有 AKS Cluster 請輸入小寫 yes) : " -i "no" list_aks_clusters
-echo ""
-[ $list_aks_clusters == yes ] && az aks list --output table
-echo ""
-
 
 # 設定 刪除 AKS 叢集名稱
 echo ""
